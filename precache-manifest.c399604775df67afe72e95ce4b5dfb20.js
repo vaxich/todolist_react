cd6056af4692ef1f55f4952c5d1b7dbe5f764a60
vaxich <vaxich@rambler.ru>
2023-01-27 19:33:23 +0300

    Updates

---
+++ precache-manifest.c399604775df67afe72e95ce4b5dfb20.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c4786cd7e27a22b70a4daa71549b3c7c",
+    "url": "/todolist_react/index.html"
+  },
+  {
+    "revision": "98a0273793f0e8f9a781",
+    "url": "/todolist_react/static/css/main.aa207b39.chunk.css"
+  },
+  {
+    "revision": "f0a93de225c63eebe658",
+    "url": "/todolist_react/static/js/2.27e3bca9.chunk.js"
+  },
+  {
+    "revision": "e465547f510146977fd4a631ac718e27",
+    "url": "/todolist_react/static/js/2.27e3bca9.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "98a0273793f0e8f9a781",
+    "url": "/todolist_react/static/js/main.8f1342f2.chunk.js"
+  },
+  {
+    "revision": "891bec73275b1715b685",
+    "url": "/todolist_react/static/js/runtime-main.6f0d22e7.js"
+  }
+]);
